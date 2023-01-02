e129f40e8350e1e16cd42ad3981a70383c179d5f
jaume-serra <jaumeserraa@gmail.com>
2023-01-02 11:47:25 +0100

    [Controllers/loadData] Update nextBirth date

--- src/controllers/loadDataController.ts
+++ src/controllers/loadDataController.ts
@@ -37,43 +37,95 @@ export const loadDataFromCSV = async (req: any, res: Response) => {
   });
   await client.db("CowProject").collection("cows").insertMany(correctData);
   await updateCowSonsFromImport(ramatId);
+  await updateNextBirth(ramatId);
   return res.json({ hola: correctData });
 };
 
 const updateCowSonsFromImport = async (ramatId: string) => {
-  await client
-    .db("CowProject")
-    .collection("cows")
-    .find({ ramatId: ramatId })
-    .forEach(function (doc) {
-      if (
-        doc.motherIdentifier &&
-        //@ts-ignore
-        client
-          .db("CowProject")
-          .collection("cows")
-          .findOne({ identifier: doc.motherIdentifier, ramatId: ramatId })
-      ) {
-        client
-          .db("CowProject")
-          .collection("cows")
-          .findOne({ identifier: doc.motherIdentifier })
-          .then((mother: any) => {
-            if (
-              !mother?.sons ||
-              !mother?.sons.find(function (son: any) {
-                return son.identifier == doc.identifier;
-              })
-            ) {
-              client
-                .db("CowProject")
-                .collection("cows")
-                .updateOne(
-                  { identifier: doc.motherIdentifier },
-                  { $push: { sons: doc } }
-                );
-            }
-          });
-      }
-    });
+  try {
+    await client
+      .db("CowProject")
+      .collection("cows")
+      .find({ ramatId: new ObjectID(ramatId) })
+      .forEach(function (doc) {
+        if (
+          doc.motherIdentifier &&
+          //@ts-ignore
+          client
+            .db("CowProject")
+            .collection("cows")
+            .findOne({
+              identifier: doc.motherIdentifier,
+              ramatId: new ObjectID(ramatId),
+            })
+        ) {
+          client
+            .db("CowProject")
+            .collection("cows")
+            .findOne({ identifier: doc.motherIdentifier })
+            .then((mother: any) => {
+              if (mother?.sons) {
+                client
+                  .db("CowProject")
+                  .collection("cows")
+                  .updateOne(
+                    { identifier: doc.motherIdentifier },
+                    { $push: { sons: doc } }
+                  );
+              }
+            });
+        }
+      });
+  } catch (err) {
+    console.log("err :>> ", err);
+  }
+};
+
+const updateNextBirth = async (ramatId: string) => {
+  try {
+    await client
+      .db("CowProject")
+      .collection("cows")
+      .find({ ramatId: new ObjectID(ramatId) })
+      .forEach(function (doc) {
+        if (
+          doc.motherIdentifier &&
+          //@ts-ignore
+          client
+            .db("CowProject")
+            .collection("cows")
+            .findOne({
+              identifier: doc.motherIdentifier,
+              ramatId: new ObjectID(ramatId),
+            })
+        ) {
+          client
+            .db("CowProject")
+            .collection("cows")
+            .findOne({ identifier: doc.motherIdentifier })
+            .then((mother) => {
+              if (mother?.sons) {
+                const lastSon = mother.sons.sort(function (a: any, b: any) {
+                  return a.birthDate > b.birthDate;
+                })[0];
+                client
+                  .db("CowProject")
+                  .collection("cows")
+                  .updateOne(
+                    { identifier: doc.motherIdentifier },
+                    {
+                      $set: {
+                        nextBirthDate: new Date(
+                          lastSon?.birthDate?.getTime() + 31536000000
+                        ),
+                      },
+                    }
+                  );
+              }
+            });
+        }
+      });
+  } catch (err) {
+    console.log("err :>> ", err);
+  }
 };
