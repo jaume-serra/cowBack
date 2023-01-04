e875c4e256730964b3dbecb7daaaf5a0511c570f
jaume-serra <jaumeserraa@gmail.com>
2023-01-04 18:46:58 +0100

    [Controllers] Upload cow image to S3 update

--- src/routes/cow.ts
+++ src/routes/cow.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { postUpdateImageCow } from "../controllers/cowController.js";
+import { uploadFile } from "../middleware/uploadFile.cjs";
 import { verifyRequest } from "../middleware/verifyRequest.js";
 
 const cowRoutes = express.Router();
 
-cowRoutes.post("/uploadImage", postUpdateImageCow);
+cowRoutes.post("/uploadImage", uploadFile.single("image"), postUpdateImageCow);
 
 export default cowRoutes;
--- src/services/s3.ts
+++ src/services/s3.ts
@@ -1,17 +1,17 @@
 import AWS from "aws-sdk";
-
+import fs from "fs";
 export const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
-export const uploadImageS3 = async (cowId: string, image: any) => {
-  const a = await s3
+export const uploadImageS3 = async (image: any) => {
+  const fileStream = fs.createReadStream(image.path);
+  await s3
     .upload({
       Bucket: "cowproject",
-      Key: cowId,
-      Body: image.uri,
+      Key: image.filename,
+      Body: fileStream,
     })
     .promise();
-  console.log("a :>> ", a);
 };
